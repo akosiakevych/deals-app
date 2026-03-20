a7007c7c910990c55f2b3fd3a0b0bacb9371678f
Artem Kosiakevych <akosiakevych@opengov.com>
2026-03-20 11:18:23 +0100

    Init

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Deals() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit the Deals screen in app/index.tsx.</Text>
+    </View>
+  );
+}
